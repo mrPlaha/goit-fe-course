62cbb6e50b48b4c3ddb7ecf01e6c4329f34c24c5
mrPlaha <studia.modern@gmail.com>
2019-06-19 13:04:17 +0300

    HW11

---
+++ JS/HW11/JS/index.js
@@ -0,0 +1,143 @@
+'use strict'
+/*
+  Реализуйте форму фильтра товаров в каталоге и список отфильтрованных товаров.
+  Используйте шаблонизацию для создания карточек товаров.
+  
+  Есть массив объектов (дальше в задании), каждый из которых описывает 
+  ноутбук с определенными характеристиками.
+  
+  Поля объекта по которым необходимо производить фильтрацию: size, color, release_date.
+  Поля объекта для отображения в карточке: name, img, descr, color, price, release_date.
+    
+  Изначально есть форма с 3-мя секциями, состоящими из заголовка и группы 
+  чекбоксов (разметка дальше в задании). После того как пользователь выбрал 
+  какие либо чекбоксы и нажал кнопку Filter, необходимо собрать значения чекбоксов по группам. 
+  
+  🔔 Подсказка: составьте объект формата
+      const filter = { size: [], color: [], release_date: [] }
+    
+  После чего выберите из массива только те объекты, которые подходят 
+  под выбраные пользователем критерии и отрендерите список карточек товаров.
+  
+  🔔 Каждый раз когда пользователь фильтрует товары, список карточек товаров очищается, 
+      после чего в нем рендерятся новые карточки товаров, соответствующих текущим критериям фильтра.
+*/
+
+const laptops = [{
+    size: 13,
+    color: 'white',
+    price: 28000,
+    release_date: 2015,
+    name: 'Macbook Air White 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 13,
+    color: 'gray',
+    price: 32000,
+    release_date: 2016,
+    name: 'Macbook Air Gray 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 13,
+    color: 'black',
+    price: 35000,
+    release_date: 2017,
+    name: 'Macbook Air Black 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'white',
+    price: 45000,
+    release_date: 2015,
+    name: 'Macbook Air White 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'gray',
+    price: 55000,
+    release_date: 2016,
+    name: 'Macbook Pro Gray 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'black',
+    price: 45000,
+    release_date: 2017,
+    name: 'Macbook Pro Black 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'white',
+    price: 65000,
+    release_date: 2015,
+    name: 'Macbook Air White 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'gray',
+    price: 75000,
+    release_date: 2016,
+    name: 'Macbook Pro Gray 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'black',
+    price: 80000,
+    release_date: 2017,
+    name: 'Macbook Pro Black 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+];
+const element = document.querySelector('#card').innerHTML.trim()
+const template = Handlebars.compile(element);
+const cards = document.querySelector('.cards');
+const form = document.querySelector('.js-form')
+const reset = document.querySelector('button[type=reset]');
+form.addEventListener('submit', filterCards)
+reset.addEventListener('click', () => {
+  cards.innerHTML = template(laptops)
+})
+let result = template(laptops);
+cards.innerHTML = result;
+
+function filterCards(evt) {
+  evt.preventDefault();
+  let filter = getArrayCheckedInputs()
+    .reduce((acc, elem) => {
+      acc[elem.name] ?
+        acc[elem.name].push(elem.value) :
+        (acc[elem.name] = [elem.value]);
+      return acc;
+    }, {});
+  result = template(getFiltered(filter, laptops));
+  cards.innerHTML = result;
+}
+
+function getArrayCheckedInputs() {
+  return Array.from(
+    form.querySelectorAll('input[type="checkbox"]:checked')
+  );
+}
+
+function getFiltered(fil, listOfObjs) {
+  return Object.keys(fil).reduce((ac, el) => {
+    return ac.filter(e => fil[el].includes('' + e[el]))
+  }, listOfObjs)
+}
